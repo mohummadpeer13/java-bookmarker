32e66515f770db24c02a62b4cdacdb49f83a62bd
peermohummad13 <mohummad.peer@icloud.com>
2024-08-13 22:38:12 +0200

    add workflows bookmarker-ui

--- bookmarker-ui/app/page.tsx
+++ bookmarker-ui/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center">
       <Navbar />
       <div className="mt-60 lg:mt-24 text-3xl font-bold">
-        Welcome to Bookmarker-api
+        Welcome to Bookmarker
       </div>
     </main>
   );
